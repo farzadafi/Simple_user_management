ce82079910235d004b36acfa8b00d540acad13ac
farzadafi <farzadafi40@gmail.com>
2024-05-21 04:25:33 +0330

    create for use main class

---
+++ src/main/java/ir/farzadafi/SimpleUserManagementApplication.java
@@ -0,0 +1,11 @@
+package ir.farzadafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleUserManagementApplication.class, args);
+    }
+}
