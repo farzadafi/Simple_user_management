600a710107636e3baba7870ca4692944f21734e8
farzadafi <farzadafi40@gmail.com>
2024-05-21 04:51:41 +0330

    create User entity

--- src/main/java/ir/farzadafi/model/User.java
+++ src/main/java/ir/farzadafi/model/User.java
@@ -1,4 +1,28 @@
 package ir.farzadafi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String nationalCode;
+    private Date birthdate;
+    private String password;
+    private LocalDateTime createdIn;
 }
