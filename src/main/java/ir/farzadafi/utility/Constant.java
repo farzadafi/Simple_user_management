0e269b932288a0bdb8cd345a49b638e733fa173f
farzadafi <farzadafi40@gmail.com>
2024-05-22 17:16:08 +0330

    add birthdate pattern and message fields

--- src/main/java/ir/farzadafi/utility/Constant.java
+++ src/main/java/ir/farzadafi/utility/Constant.java
@@ -7,4 +7,10 @@ public class Constant {
 
     public static final String PASSWORD_MESSAGE =
             "Minimum eight char, at least one uppercase, one lowercase letter, one number and one special character";
+
+    public static final String BIRTHDATE_PATTERN =
+            "^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$";
+
+    public static final String BIRTHDATE_MESSAGE =
+            "please send a valid birth date -> 1999/10/10";
 }
