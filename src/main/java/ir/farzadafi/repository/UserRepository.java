ee7633b798c39c1e891aafb8bfdf8aab5135a315
farzadafi <farzadafi40@gmail.com>
2024-05-22 16:13:53 +0330

    add updatePassword method

--- src/main/java/ir/farzadafi/repository/UserRepository.java
+++ src/main/java/ir/farzadafi/repository/UserRepository.java
@@ -2,6 +2,13 @@ package ir.farzadafi.repository;
 
 import ir.farzadafi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Modifying
+    @Query("UPDATE User u SET u.password = :newPassword WHERE u.id = :id")
+    void updatePassword(@Param("newPassword") String newPassword, @Param("id") int id);
 }
