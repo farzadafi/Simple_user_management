a222bc121aa2f78d84d9ffd08d2ee8d2575f98ac
farzadafi <farzadafi40@gmail.com>
2024-05-22 15:09:54 +0330

    create create_table.sql file and add command for user table

---
+++ src/main/java/ir/farzadafi/sql/create_table.sql
@@ -0,0 +1,15 @@
+USE `user-management`;
+
+CREATE TABLE user
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    firstname     VARCHAR(255),
+    lastname      VARCHAR(255),
+    national_code VARCHAR(255),
+    email         VARCHAR(255),
+    password      VARCHAR(255),
+    birthdate     DATE,
+    created_in    DATETIME,
+    CONSTRAINT UK_national_code UNIQUE (national_code),
+    CONSTRAINT UK_email UNIQUE (email)
+);
