3cd2c163765be88efa9bd0fe1fee0d76654256e0
farzadafi <farzadafi40@gmail.com>
2024-05-22 14:35:21 +0330

    change strategy for id from AUTO TO IDENTITY

--- src/main/java/ir/farzadafi/model/User.java
+++ src/main/java/ir/farzadafi/model/User.java
@@ -17,7 +17,7 @@ import java.util.Date;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstname;
     private String lastname;
