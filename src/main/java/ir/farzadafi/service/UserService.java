693ebfdd2313c1ee668aed6e4e46751b2ca04336
farzadafi <farzadafi40@gmail.com>
2024-05-22 15:48:46 +0330

    add update method

--- src/main/java/ir/farzadafi/service/UserService.java
+++ src/main/java/ir/farzadafi/service/UserService.java
@@ -1,11 +1,14 @@
 package ir.farzadafi.service;
 
+import ir.farzadafi.dto.UserUpdateRequestDto;
 import ir.farzadafi.exception.InformationDuplicateException;
 import ir.farzadafi.model.User;
 import ir.farzadafi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +28,14 @@ public class UserService {
                         (String.format("%s email is duplicate", user.getEmail()));
         }
         return user;
+    }
+
+    public User update(UserUpdateRequestDto newUserInformation) {
+        // TODO: 22.05.24 find user from DB or spring security, nationalCode and email
+        User user = new User();
+        Optional.ofNullable(newUserInformation.firstname()).ifPresent(user::setFirstname);
+        Optional.ofNullable(newUserInformation.lastname()).ifPresent(user::setLastname);
+        Optional.ofNullable(newUserInformation.birthdate()).ifPresent(user::setBirthdate);
+        return userRepository.save(user);
     }
 }
