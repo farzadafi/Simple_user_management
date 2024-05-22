a42747d99041bfc583b82d9f7f2fc37515c10979
farzadafi <farzadafi40@gmail.com>
2024-05-22 15:14:27 +0330

    create InformationDuplicateException class and extends from RunTimeException

---
+++ src/main/java/ir/farzadafi/exception/InformationDuplicateException.java
@@ -0,0 +1,8 @@
+package ir.farzadafi.exception;
+
+public class InformationDuplicateException extends RuntimeException {
+
+    public InformationDuplicateException(String message) {
+        super(message);
+    }
+}
