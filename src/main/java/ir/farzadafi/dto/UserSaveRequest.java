c6d874942fd578e9151f93eac6286cd6e32068cf
farzadafi <farzadafi40@gmail.com>
2024-05-22 20:49:25 +0330

    write some config for swagger

--- src/main/java/ir/farzadafi/dto/UserSaveRequest.java
+++ src/main/java/ir/farzadafi/dto/UserSaveRequest.java
@@ -1,21 +1,34 @@
 package ir.farzadafi.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import ir.farzadafi.validation.NationalCode;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 
 import static ir.farzadafi.utility.Constant.*;
 
-public record UserSaveRequest(String firstname,
-                              String lastname,
-                              @NationalCode
-                              String nationalCode,
-                              @Email
-                              String email,
-                              @Pattern(regexp = PASSWORD_PATTERN, message = PASSWORD_MESSAGE)
-                              String password,
-                              @Pattern(regexp = BIRTHDATE_PATTERN, message = BIRTHDATE_MESSAGE)
-                              @JsonFormat(pattern = "yyyy-MM-dd")
-                              String birthdate) {
+public record UserSaveRequest(
+        @Schema(example = "farzad")
+        String firstname,
+
+        @Schema(example = "afshar")
+        String lastname,
+
+        @NationalCode
+        @Schema(example = "3080252***")
+        String nationalCode,
+
+        @Email
+        @Schema(example = "farzadafi40@gmail.com")
+        String email,
+
+        @Pattern(regexp = PASSWORD_PATTERN, message = PASSWORD_MESSAGE)
+        @Schema(example = "aaaAAA111!!!")
+        String password,
+
+        @Pattern(regexp = BIRTHDATE_PATTERN, message = BIRTHDATE_MESSAGE)
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        @Schema(example = "1997-10-10")
+        String birthdate) {
 }
