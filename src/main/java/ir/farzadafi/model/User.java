4adf4c17013aa1a0e1708a5bfd3ca8d64fa042e1
farzadafi <farzadafi40@gmail.com>
2024-05-22 15:15:15 +0330

    add some constraint to column

--- src/main/java/ir/farzadafi/model/User.java
+++ src/main/java/ir/farzadafi/model/User.java
@@ -1,10 +1,8 @@
 package ir.farzadafi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -15,15 +13,31 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "UK_national_code", columnNames = "national_code"),
+        @UniqueConstraint(name = "UK_email", columnNames = "email")
+})
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String firstname;
+
     private String lastname;
+
+    @Column(unique = true, name = "national_code")
     private String nationalCode;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
+
     private Date birthdate;
+
+    @Column(name = "created_in")
+    @CreationTimestamp
     private LocalDateTime createdIn;
 }
