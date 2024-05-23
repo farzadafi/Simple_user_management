d94abc25a7a4bb24868d7b2af3292f933b785a95
farzadafi <farzadafi40@gmail.com>
2024-05-23 18:51:04 +0330

    add all needed attribute

--- src/main/java/ir/farzadafi/dto/UserSaveResponse.java
+++ src/main/java/ir/farzadafi/dto/UserSaveResponse.java
@@ -1,4 +1,10 @@
 package ir.farzadafi.dto;
 
-public record UserSaveResponse() {
+public record UserSaveResponse(
+        Integer id,
+        String firstname,
+        String lastname,
+        String nationalCode,
+        String email,
+        String birthdate) {
 }
