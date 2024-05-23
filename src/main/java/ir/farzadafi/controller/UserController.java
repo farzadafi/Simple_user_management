5424a0c0255fcf9391a8ee34075f5bc8a94bae33
farzadafi <farzadafi40@gmail.com>
2024-05-23 18:57:28 +0330

    add update method

--- src/main/java/ir/farzadafi/controller/UserController.java
+++ src/main/java/ir/farzadafi/controller/UserController.java
@@ -2,6 +2,8 @@ package ir.farzadafi.controller;
 
 import ir.farzadafi.dto.UserSaveRequest;
 import ir.farzadafi.dto.UserSaveResponse;
+import ir.farzadafi.dto.UserUpdateRequest;
+import ir.farzadafi.dto.UserUpdateResponse;
 import ir.farzadafi.mapper.UserMapper;
 import ir.farzadafi.model.User;
 import ir.farzadafi.service.UserService;
@@ -9,10 +11,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -27,5 +26,12 @@ public class UserController {
         User savedUser = userService.save(user);
         UserSaveResponse userSaveResponse = UserMapper.INSTANCE.modelToResponse(savedUser);
         return new ResponseEntity<>(userSaveResponse, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public UserUpdateResponse update(@Valid @RequestBody UserUpdateRequest request) {
+        User user = UserMapper.INSTANCE.updateRequestToModel(request);
+        User updatedUser = userService.update(user);
+        return UserMapper.INSTANCE.modelToUpdateResponse(updatedUser);
     }
 }
