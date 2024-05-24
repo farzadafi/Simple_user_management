27e0f4f4e643d7ddcd3d19fa0b9b3a49060a6b34
farzadafi <farzadafi40@gmail.com>
2024-05-24 13:09:10 +0330

    create MailConfig class

---
+++ src/main/java/ir/farzadafi/config/MailConfig.java
@@ -0,0 +1,28 @@
+package ir.farzadafi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setProtocol("smtp");
+        return mailSender;
+    }
+}
