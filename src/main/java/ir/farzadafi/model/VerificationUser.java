6a261b9ecddaffd9faee48cdb80c9a5254c219d7
farzadafi <farzadafi40@gmail.com>
2024-05-24 14:20:03 +0330

    create VerificationUser class

---
+++ src/main/java/ir/farzadafi/model/VerificationUser.java
@@ -0,0 +1,31 @@
+package ir.farzadafi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class VerificationUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private User user;
+
+    private int code;
+
+    @CreationTimestamp
+    private LocalDateTime created_in;
+
+    public VerificationUser(User user, int code) {
+        this.user = user;
+        this.code = code;
+    }
+}
