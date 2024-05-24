49303d96827a86840b32fd97fbee5095bee3ce29
farzadafi <farzadafi40@gmail.com>
2024-05-24 19:25:33 +0330

    fix verificationUser method

--- src/main/java/ir/farzadafi/service/VerificationUserService.java
+++ src/main/java/ir/farzadafi/service/VerificationUserService.java
@@ -4,6 +4,7 @@ import ir.farzadafi.exception.NotFoundException;
 import ir.farzadafi.exception.TokenExpireException;
 import ir.farzadafi.model.VerificationUser;
 import ir.farzadafi.repository.VerificationUserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +15,13 @@ public class VerificationUserService {
     private final VerificationUserRepository repository;
     private final UserService userService;
 
+    @Transactional
     public void verificationUser(int code) {
         VerificationUser verificationUser = repository.findByCode(code).orElseThrow(
                 () -> new NotFoundException("please register again"));
         checkVerificationTime(verificationUser);
         userService.enable(verificationUser.getId());
-        repository.delete(verificationUser);
+        repository.customDelete(verificationUser.getId());
     }
 
     private void checkVerificationTime(VerificationUser verificationUser) {
