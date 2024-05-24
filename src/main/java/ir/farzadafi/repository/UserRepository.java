08073c400d95e9c7aa5462500675ccad11f5916f
farzadafi <farzadafi40@gmail.com>
2024-05-24 18:22:22 +0330

    add findByEmail method

--- src/main/java/ir/farzadafi/repository/UserRepository.java
+++ src/main/java/ir/farzadafi/repository/UserRepository.java
@@ -6,9 +6,17 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Modifying
     @Query("UPDATE User u SET u.password = :newPassword WHERE u.id = :id")
     void updatePassword(@Param("newPassword") String newPassword, @Param("id") int id);
+
+    @Modifying
+    @Query("UPDATE User u SET u.enabled = true WHERE u.id = :id")
+    void enable(@Param("id") int id);
+
+    Optional<User> findByEmail(String email);
 }
