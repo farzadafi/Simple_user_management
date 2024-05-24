8a3f216b60996c2e5e28c9694e4e4155796feaeb
farzadafi <farzadafi40@gmail.com>
2024-05-24 14:19:12 +0330

    fix save method and add sendEmail method

--- src/main/java/ir/farzadafi/service/UserService.java
+++ src/main/java/ir/farzadafi/service/UserService.java
@@ -6,6 +6,9 @@ import ir.farzadafi.model.User;
 import ir.farzadafi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,19 +18,36 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JavaMailSender javaMailSender;
 
     public User save(User user) {
         try {
             user = userRepository.save(user);
+            sendEmail(user.getEmail(), user.getVerificationUser().getCode());
         } catch (DataIntegrityViolationException e) {
-            if (e.getMessage().contains("user.UK_national_code"))
-                throw new InformationDuplicateException
-                        (String.format("%s national code is duplicate", user.getNationalCode()));
-            if (e.getMessage().contains("user.UK_email"))
-                throw new InformationDuplicateException
-                        (String.format("%s email is duplicate", user.getEmail()));
+            handleSaveException(e.getMessage(), user.getNationalCode(), user.getEmail());
         }
         return user;
+    }
+
+    private void handleSaveException(String message, String nationalCode, String email) {
+        if (message.contains("user.UK_national_code"))
+            throw new InformationDuplicateException
+                    (String.format("%s national code is duplicate", nationalCode));
+        if (message.contains("user.UK_email"))
+            throw new InformationDuplicateException
+                    (String.format("%s email is duplicate", email));
+    }
+
+    private void sendEmail(String to, int code) {
+        String body = "<p>your verification code is <b>" + code + "<b/><p/>";
+        MimeMessagePreparator message = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(to);
+            messageHelper.setSubject("Email verification");
+            messageHelper.setText(body, true);
+        };
+        javaMailSender.send(message);
     }
 
     public User update(User newUserInformation) {
@@ -45,7 +65,7 @@ public class UserService {
         userRepository.updatePassword(request.getNewPassword(), user.getId());
     }
 
-    public void remove(int id){
+    public void remove(int id) {
         // TODO: 23.05.24 fetch id from spring security
         userRepository.deleteById(id);
     }
