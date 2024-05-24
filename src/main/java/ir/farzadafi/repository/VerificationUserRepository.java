2f720cc1baa808b20f34ad2f506e035bb1560880
farzadafi <farzadafi40@gmail.com>
2024-05-24 19:25:12 +0330

    add customDelete method

--- src/main/java/ir/farzadafi/repository/VerificationUserRepository.java
+++ src/main/java/ir/farzadafi/repository/VerificationUserRepository.java
@@ -2,9 +2,16 @@ package ir.farzadafi.repository;
 
 import ir.farzadafi.model.VerificationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface VerificationUserRepository extends JpaRepository<VerificationUser, Integer> {
     Optional<VerificationUser> findByCode(int code);
+
+    @Modifying
+    @Query("DELETE FROM VerificationUser v WHERE v.id = :id")
+    void customDelete(@Param("id") int id);
 }
