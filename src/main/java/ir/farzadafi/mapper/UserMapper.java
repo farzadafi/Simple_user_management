1e035eae0dc2fb1558da8a3930f483f4d833cd13
farzadafi <farzadafi40@gmail.com>
2024-05-25 21:08:23 +0330

    fix requestToModel and add some mapping

--- src/main/java/ir/farzadafi/mapper/UserMapper.java
+++ src/main/java/ir/farzadafi/mapper/UserMapper.java
@@ -6,6 +6,8 @@ import ir.farzadafi.dto.UserUpdateRequest;
 import ir.farzadafi.dto.UserUpdateResponse;
 import ir.farzadafi.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -13,6 +15,11 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mappings({
+            @Mapping(source = "address.provinceId", target = "address.province.id"),
+            @Mapping(source = "address.countyId", target = "address.county.id"),
+            @Mapping(source = "address.cityId", target = "address.city.id"),
+    })
     User requestToModel(UserSaveRequest userSaveRequest);
 
     UserSaveResponse modelToResponse(User user);
