dd37f5558f0cea072b299a16720eca4d8f61956b
farzadafi <farzadafi40@gmail.com>
2024-05-26 16:52:03 +0330

    extends from specifiaction interface

--- src/main/java/ir/farzadafi/repository/UserRepository.java
+++ src/main/java/ir/farzadafi/repository/UserRepository.java
@@ -2,13 +2,14 @@ package ir.farzadafi.repository;
 
 import ir.farzadafi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
     @Modifying
     @Query("UPDATE User u SET u.password = :newPassword WHERE u.id = :id")
