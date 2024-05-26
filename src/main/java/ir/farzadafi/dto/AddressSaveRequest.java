c928dc6f98d5e88c45f4a7cd2c7c126697a7c4bf
farzadafi <farzadafi40@gmail.com>
2024-05-26 04:43:03 +0330

    write some example for each field

--- src/main/java/ir/farzadafi/dto/AddressSaveRequest.java
+++ src/main/java/ir/farzadafi/dto/AddressSaveRequest.java
@@ -1,9 +1,20 @@
 package ir.farzadafi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
-public record AddressSaveRequest(@NotNull(message = "province can't be null") Integer provinceId,
-                                 @NotNull(message = "county can't be null") Integer countyId,
-                                 @NotNull(message = "city can't be null") Integer cityId,
-                                 String street) {
+public record AddressSaveRequest(
+        @NotNull(message = "province can't be null")
+        @Schema(example = "1")
+        Integer provinceId,
+
+        @NotNull(message = "county can't be null")
+        @Schema(example = "2")
+        Integer countyId,
+
+        @NotNull(message = "city can't be null")
+        @Schema(example = "3")
+        Integer cityId,
+
+        String street) {
 }
