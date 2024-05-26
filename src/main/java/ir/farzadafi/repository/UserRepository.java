420f1941f874eba4dce1c4e8108679020afd5dc8
farzadafi <farzadafi40@gmail.com>
2024-05-26 21:19:18 +0330

    add findByNationalCode method

--- src/main/java/ir/farzadafi/repository/UserRepository.java
+++ src/main/java/ir/farzadafi/repository/UserRepository.java
@@ -24,4 +24,6 @@ public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecifi
     Optional<User> findByEmail(String email);
 
     Page<User> findAll(Pageable pageable);
+
+    Optional<User> findByNationalCode(String nationalCode);
 }
