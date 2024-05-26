453798f91b0868ceb099586756b58cba7f0bf79f
farzadafi <farzadafi40@gmail.com>
2024-05-26 03:48:42 +0330

    add command for create location_hierarchy and address table

--- src/main/java/ir/farzadafi/sql/create_table.sql
+++ src/main/java/ir/farzadafi/sql/create_table.sql
@@ -1,6 +1,6 @@
 USE `user-management`;
 
-CREATE TABLE user
+CREATE TABLE IF NOT EXISTS user
 (
     id            INT AUTO_INCREMENT PRIMARY KEY,
     firstname     VARCHAR(255),
@@ -10,8 +10,24 @@ CREATE TABLE user
     password      VARCHAR(255),
     birthdate     DATE,
     created_in    DATETIME,
-    enabled     TINYINT,
+    enabled       TINYINT,
     deleted       TINYINT,
     CONSTRAINT UK_national_code UNIQUE (national_code),
     CONSTRAINT UK_email UNIQUE (email)
 );
+
+CREATE TABLE IF NOT EXISTS location_hierarchy
+(
+    id                    INT AUTO_INCREMENT PRIMARY KEY,
+    name                  VARCHAR(255),
+    location_hierarchy_id INT
+);
+
+CREATE TABLE IF NOT EXISTS address
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    street      VARCHAR(255),
+    province_id INT REFERENCES location_hierarchy (id),
+    county_id   INT REFERENCES location_hierarchy (id),
+    city_id     INT REFERENCES location_hierarchy (id)
+)
