0932e4dc10894f2b02eda584657b292fb663b7ba
farzadafi <farzadafi40@gmail.com>
2024-07-06 21:24:28 +0330

    add needed annotation

--- src/main/java/ir/farzadafi/model/jaksonClass/SubClassA.java
+++ src/main/java/ir/farzadafi/model/jaksonClass/SubClassA.java
@@ -1,4 +1,14 @@
 package ir.farzadafi.model.jaksonClass;
 
-public class SubClassA {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubClassA extends BaseClass {
+    private String firstString;
 }
