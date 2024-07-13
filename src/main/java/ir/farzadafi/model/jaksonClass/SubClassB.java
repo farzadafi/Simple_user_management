05d30f5907d746b653d55e358ce6857d848819e2
farzadafi <farzadafi40@gmail.com>
2024-07-13 05:38:19 +0330

    add needed field and add lombok annotation

--- src/main/java/ir/farzadafi/model/jaksonClass/SubClassB.java
+++ src/main/java/ir/farzadafi/model/jaksonClass/SubClassB.java
@@ -1,4 +1,14 @@
 package ir.farzadafi.model.jaksonClass;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubClassB extends BaseClass {
+    private String secondString;
 }
