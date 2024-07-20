47624caafffdc3017b7f6bcbc18c9f6ae301f4f4
mahboobdorali <mahboobdorali@gmail.com>
2024-07-20 05:17:15 +0330

    create UserServiceTest class

---
+++ src/test/java/ir/farzadafi/UserServiceTest.java
@@ -0,0 +1,8 @@
+package ir.farzadafi;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+}
