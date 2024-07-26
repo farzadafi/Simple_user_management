dc71cc2d6ce7087c73eea6537c218f51386d96b1
farzadafi <farzadafi40@gmail.com>
2024-07-26 07:48:29 +0330

    add Json Annotation for define sub class

--- src/main/java/ir/farzadafi/model/jaksonClass/BaseClass.java
+++ src/main/java/ir/farzadafi/model/jaksonClass/BaseClass.java
@@ -1,5 +1,6 @@
 package ir.farzadafi.model.jaksonClass;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,5 +12,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = BaseClass.class)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = SimpleSubClass.class, names = "SimpleSubClass"),
+        @JsonSubTypes.Type(value = SubClassA.class, names = "SubClassA"),
+        @JsonSubTypes.Type(value = SubClassB.class, names = "SubClassB")
+})
 public abstract class BaseClass {
+    private String name;
 }
