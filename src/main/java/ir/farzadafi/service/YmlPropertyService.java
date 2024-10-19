3decaa2f5491157857ce1f05415f2b1a96e44f69
farzadafi <farzadafi40@gmail.com>
2024-10-19 20:59:35 +0330

    create getAllFiles method

--- src/main/java/ir/farzadafi/service/YmlPropertyService.java
+++ src/main/java/ir/farzadafi/service/YmlPropertyService.java
@@ -1,5 +1,31 @@
 package ir.farzadafi.service;
 
+import java.io.File;
+import java.util.List;
+
+import ir.farzadafi.exception.DirectoryException;
+
 public class YmlPropertyService{
 
+  private String localConfigAddress;
+
+  public List<String> getAllYmlFileNames() {
+    File[] files = getAllFiles();
+    return null;
+  }
+
+  private File[] getAllFiles() {
+    File directory = new File(localConfigAddress);
+    if (!directory.exists())
+      throw new DirectoryException("localConfig directory Not Found!");
+    if(!directory.isDirectory())
+      throw new DirectoryException("localConfig Address is not a dirctory!");
+
+    File[] listFiles = directory.listFiles();
+    if(listFiles == null)
+      throw new DirectoryException("Something is wrong");
+    if(listFiles.length == 0)
+      throw new DirectoryException("localConfig directory is Empty!");
+    return listFiles;
+  }
 }
