a79dae8dfbcd1186c51b779d7bc6a9642f148516
farzadafi <farzadafi40@gmail.com>
2024-10-19 20:58:56 +0330

    extend from RunTimeException and create one constructor

--- src/main/java/ir/farzadafi/exception/DirectoryException.java
+++ src/main/java/ir/farzadafi/exception/DirectoryException.java
@@ -1,6 +1,9 @@
-package ir.farzadafi.service;
+package ir.farzadafi.exception;
 
-public class DirectoryException {
+public class DirectoryException extends RuntimeException{
 
+    public DirectoryException(String message) {
+      super(message);
+    }
 }
 
