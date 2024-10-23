0649fd373cffb961a157cd1bd583c61587200acc
farzadafi <farzadafi40@gmail.com>
2024-10-23 22:21:45 +0330

    add method for return all file under resources dir

--- src/main/java/ir/farzadafi/service/YmlPropertyService.java
+++ src/main/java/ir/farzadafi/service/YmlPropertyService.java
@@ -1,11 +1,25 @@
 package ir.farzadafi.service;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import ir.farzadafi.exception.DirectoryException;
 
 @Service
 public class YmlPropertyService{
+
+  public File[] returnAllFileUnderResourcesDir() {
+    File[] files = null;
+    try{
+      files = ResourceUtils.getFile("classpath:").listFiles();
+    }catch(IOException e) {
+      throw new DirectoryException("find files under resources occur exception");
+    }
+    return files;
+  }
 
   private boolean isYmlFile(File file) {
     String name = file.getName();
