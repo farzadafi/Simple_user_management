d95243cc4f17f6620e8eac952ecf367d69f7e9c5
farzadafi <farzadafi40@gmail.com>
2024-10-24 09:20:02 +0330

    create for main class

---
+++ simple_yml_service/src/main/java/ir/farzadafi/SimpleYmlServiceApplication.java
@@ -0,0 +1,11 @@
+package ir.farzadafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleYmlServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleYmlServiceApplication.class, args);
+  }
+}
