015f2737cd0de71e2fa74d8934353a362a22d5d5
farzadafi <farzadafi40@gmail.com>
2024-11-01 21:58:12 +0330

    create new controller for yml property

---
+++ simple_yml_service/src/main/java/ir/farzadafi/controller/YmlPropertyController.java
@@ -0,0 +1,10 @@
+package ir.farzadafi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/yml")
+public class YmlPropertyController{
+
+}
